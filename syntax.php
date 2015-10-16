e79a680195d679173f4b6a76edf8b825f1ac2643
matzekuh <matzekuh@users.noreply.github.com>
2015-10-16 15:23:32 +0200

    Update syntax.php

--- syntax.php
+++ syntax.php
@@ -196,20 +196,20 @@
                 $table = "<table>
                         <thead>
                             <tr class='row0'>
-                                <th class="col0"> Name </th>
-                                <th class="col1"> Address </th>
-                                <th class="col2"> Country </th>
-                                <th class="col3"> Phone number </th>
-                                <th class="col4"> Email address </th>
+                                <th class='col0'> Name </th>
+                                <th class='col1'> Address </th>
+                                <th class='col2'> Country </th>
+                                <th class='col3'> Phone number </th>
+                                <th class='col4'> Email address </th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr class='row1'>
-                                <th class="col0"> ".$data['name']." </th>
-                                <th class="col1"> ".$data['adress']." </th>
-                                <th class="col2"> ".$data['country']." </th>
-                                <th class="col3"> ".$data['phone']." </th>
-                                <th class="col4"> ".$data['email']." </th>
+                                <th class='col0'> ".$data['name']." </th>
+                                <th class='col1'> ".$data['adress']." </th>
+                                <th class='col2'> ".$data['country']." </th>
+                                <th class='col3'> ".$data['phone']." </th>
+                                <th class='col4'> ".$data['email']." </th>
                             </tr>
                         </tbody>
                     </table>";
